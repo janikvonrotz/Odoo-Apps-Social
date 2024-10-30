2ec6244a71127b1d92abadbb7f39d7df4082facc
Janik von Rotz <login@janikvonrotz.ch>
2024-10-30 16:19:33 +0100

    feat: lint

--- README.md
+++ README.md
@@ -15,5 +15,8 @@ git clone git@github.com:mint-system/odoo-apps-social.git ./addons/social
 | Module | Summary |
 | --- | --- |
 | [account_move_unsubscribe](account_move_unsubscribe) |         Unsubscribe all followers after account move post. |
+| [mail_composer_mass_layout](mail_composer_mass_layout) |         Render layout in mass composition mode. |
 | [mail_no_autofollow](mail_no_autofollow) |         This disables autofollow on message post. |
 | [mail_unsubscribe](mail_unsubscribe) |         Auto unsubscribe followers before sending a message. |
+| [purchase_order_unsubscribe](purchase_order_unsubscribe) |         Unsubscribe all followers after purchase order confirmation. |
+| [sale_order_unsubscribe](sale_order_unsubscribe) |         Unsubscribe followers after sale order confirmation. |
--- mail_composer_mass_layout/wizard/mail_compose_message.py
+++ mail_composer_mass_layout/wizard/mail_compose_message.py
@@ -9,26 +9,41 @@ class MailComposer(models.TransientModel):
     _inherit = "mail.compose.message"
 
     def render_message(self, res_ids):
-        template_values = super(MailComposer, self).render_message(res_ids)
+        """
+        This method returns the email values including the mail body.
+        It iterates on the records and updates the mail body with the layout.
+        """
+        template_values = super().render_message(res_ids)
 
-        for value in template_values:
-            _logger.warning(value)
+        for res_id in res_ids:
+            template_value = template_values[res_id]
 
-            # record = self.env[self.model].browse(res_id)
-            # model = self.env['ir.model']._get(record._name)
-            # template_ctx = {
-            #     'message': self.env['mail.message'].sudo().new(dict(body=bodies[res_id], record_name=record.display_name)),
-            #     'subtype': self.env['mail.message.subtype'].sudo(),
-            #     'model_description': model.display_name,
-            #     'record': record,
-            #     'record_name': False,
-            #     'subtitles': False,
-            #     'company': 'company_id' in record and record['company_id'] or self.env.company,
-            #     'email_add_signature': False,
-            #     'signature': '',
-            #     'website_url': '',
-            # }
-            # body =  self.env['ir.qweb']._render('mail.mail_notification_light', template_ctx, minimal_qcontext=True, raise_if_not_found=False)
-            # bodies[res_id] = body
+            # Get record and model
+            record = self.env[self.model].browse(res_id)
+            model = self.env["ir.model"]._get(record._name)
+
+            # Prepare template
+            template_ctx = {
+                "message": self.env["mail.message"]
+                .sudo()
+                .new(
+                    dict(body=template_value["body"], record_name=record.display_name)
+                ),
+                "model_description": model.display_name,
+                "company": "company_id" in record
+                and record["company_id"]
+                or self.env.company,
+            }
+
+            # Render template with layout
+            body = model.env["ir.qweb"]._render(
+                self.email_layout_xmlid,
+                template_ctx,
+                minimal_qcontext=True,
+                raise_if_not_found=False,
+            )
+
+            # Update with new body
+            template_values[res_id]["body"] = body
 
         return template_values
