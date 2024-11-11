67be2092b6771394fb7ff652e899309ca1b7db6f
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-11-11 13:07:44 +0100

    feat: init module upgrade

---
+++ account_move_mail_thread/models/account_move.py
@@ -0,0 +1,22 @@
+import logging
+
+from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    # odoo/addons/mail/models/mail_thread.py
+    @api.model_create_multi
+    def create(self, vals_list):
+        # _logger.warning('Call create method without log and tracking.')
+        return super(AccountMove, self.with_context(tracking_disable=True)).create(
+            vals_list
+        )
+
+    # odoo/addons/sale/wizard/sale_make_invoice_advance.py
+    def message_post_with_view(self, views_or_xmlid, **kwargs):
+        # _logger.warning('Do not call message post with view method.')
+        return
